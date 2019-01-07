2f6541002921efcfe119bc5c88cdde1d3412d201
KseeleK <939336352@qq.com>
2019-01-07 22:32:00 +0800

    dubug10

--- config/debug.h
+++ config/debug.h
@@ -1,29 +1,29 @@
-// ps2: display intermedia info
-// #define PS2_DEBUG
+// // ps2: display intermedia info
+// // #define PS2_DEBUG
 
-// sd driver: display hardware-related info
-// #define SD_DEBUG
+// // sd driver: display hardware-related info
+// // #define SD_DEBUG
 
-// vga: calibrate vga output
-// #define VGA_CALIBRATE
+// // vga: calibrate vga output
+// // #define VGA_CALIBRATE
 
-// memset
-// #define MEMSET_DEBUG
+// // memset
+// // #define MEMSET_DEBUG
 
-// boot mem
-// #define BOOTMM_DEBUG
+// // boot mem
+// // #define BOOTMM_DEBUG
 
-// slab: display debug info
-#define SLAB_DEBUG
-#define BUDDY_DEBUG
+// // slab: display debug info
+// #define SLAB_DEBUG
+// #define BUDDY_DEBUG
 
-// myvi: display debug info
-// #define MYVI_DEBUG
+// // myvi: display debug info
+// // #define MYVI_DEBUG
 
-// file system
-// #define FS_DEBUG
+// // file system
+// // #define FS_DEBUG
 
-// exec
-#define EXEC_DEBUG
+// // exec
+// #define EXEC_DEBUG
 
-#define TLB_DEBUG
+// #define TLB_DEBUG
